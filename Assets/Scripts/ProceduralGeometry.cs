07a0d262c73cad198e09596e99fdf4e2e1741e29
Etienne Cella <etiennec@MacBook-Pro-3.local>
2019-08-24 13:27:38 -0400

    YOLO

---
+++ Assets/Scripts/ProceduralGeometry.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(MeshFilter))]
+public class ProceduralGeometry : MonoBehaviour 
+{
+    [SerializeField, Range(0, 180)] float alpha;
+    [SerializeField, Range(0, 180)] float beta;
+    [SerializeField, Range(0, 20)] float radius;
+    [SerializeField] int xResolution;
+    [SerializeField] int yResolution;
+    [SerializeField] Material mat;
+    [SerializeField] Transform projectionPlane;
+    [SerializeField] Transform projectionCamera;
+
+    Mesh m_Mesh;
+
+    private void OnEnable()
+    {
+        UpdateGeometry();
+        UpdateMaterial();
+    }
+
+    private void OnDisable()
+    {
+        DestroyImmediate(m_Mesh);
+    }
+
+    private void OnValidate()
+    {
+        xResolution = Mathf.Max(1, xResolution);
+        yResolution = Mathf.Max(1, yResolution);
+        UpdateGeometry();
+        UpdateMaterial();
+    }
+
+    void Update()
+    {
+        UpdateMaterial();
+    }
+
+    void UpdateMaterial()
+    {
+        // TODO: optimization, do not manipulate uniform string names
+        mat.SetFloat("_Radius", radius);
+        mat.SetFloat("_AlphaRad", alpha * Mathf.Deg2Rad);
+        mat.SetFloat("_BetaRad", beta * Mathf.Deg2Rad);
+        mat.SetVector("_PlanePoint", projectionPlane.position);
+        mat.SetVector("_PlaneNormal", projectionPlane.up);
+        mat.SetMatrix("_WorldToPlane", projectionPlane.worldToLocalMatrix);
+        mat.SetVector("_EyePosition", projectionCamera.position);
+    }
+
+    Mesh GetOrCreateMesh()
+    {
+        if (m_Mesh != null)
+            return m_Mesh;
+        
+        m_Mesh = new Mesh();
+        m_Mesh.hideFlags = HideFlags.HideAndDontSave;
+        var filter = GetComponent<MeshFilter>();
+        filter.mesh = m_Mesh;
+        return m_Mesh;
+    }
+
+    void UpdateGeometry()
+    {
+        var numVerts = (xResolution + 1) * (yResolution + 1);
+        var dx = 1.0f / (float)xResolution;
+        var dy = 1.0f / (float)yResolution;
+        var numTris = xResolution  * yResolution * 2;
+        var vertices = new Vector3[numVerts];
+        var indices = new int[numTris * 3];
+
+        for (var y = 0; y != yResolution + 1; ++y)
+        {
+            for (var x = 0; x != xResolution + 1; ++x)
+            {
+                vertices[y * (xResolution + 1) + x] = Quaternion.Euler(
+                    (y * dy - 0.5f) * beta, 
+                    (x * dx - 0.5f) * alpha, 0) * Vector3.forward * radius;
+            }
+        }
+
+        var index = 0;
+        var xResPlusOne = xResolution + 1;
+        for (var x = 0; x != xResolution; ++x)
+        {
+            for (var y = 0; y != yResolution; ++y)
+            {
+                indices[index++] = y * xResPlusOne + x;
+                indices[index++] = y * xResPlusOne + x + 1;
+                indices[index++] = (y + 1) * xResPlusOne + x + 1;
+
+                indices[index++] = (y + 1) * xResPlusOne + x + 1;
+                indices[index++] = (y + 1) * xResPlusOne + x;
+                indices[index++] = y * xResPlusOne + x;
+            }
+        }
+
+        var mesh = GetOrCreateMesh();
+        mesh.Clear();
+        mesh.vertices = vertices;
+        mesh.triangles = indices;
+    }
+}
---
+++ Assets/Shaders/Project.shader
@@ -0,0 +1,71 @@
+﻿Shader "Unlit/Project"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+        _Scale("Scale", Float) = 0.05
+    }
+    SubShader
+    {
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+                float3 worldPos : TEXCOORD1;
+            };
+
+            sampler2D _MainTex;
+            float _Radius;
+            float _AlphaRad;
+            float _BetaRad;
+            
+            float3 _PlaneNormal;
+            float3 _PlanePoint;
+            float4x4 _WorldToPlane;
+            
+            float3 _EyePosition;
+            float _Scale;
+            
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                // projection will be computed in world space
+                o.worldPos = mul(unity_ObjectToWorld, v.vertex);
+                o.uv = v.uv;
+                return o;
+            }
+            
+            float rayPlaneIntersection(float3 rayOrigin, float3 rayDir, float3 planePoint, float3 planeNormal)
+            {
+                return dot(planePoint - rayOrigin, planeNormal) / dot(rayDir, planeNormal);
+            }
+            
+            fixed4 frag (v2f i) : SV_Target
+            {
+                float3 eyeDir = normalize(i.worldPos - _EyePosition);
+                float t = rayPlaneIntersection(_EyePosition, eyeDir, _PlanePoint, _PlaneNormal);
+                float3 planeIntersect = _EyePosition + eyeDir * t;
+                // convert to plane coordinates
+                float4 planeCoords = mul(_WorldToPlane, planeIntersect);
+                float2 uv = saturate(planeCoords.xz * float2(1, -1) * _Scale + 0.5) - 0.5;
+                return tex2D(_MainTex, uv);
+            }
+            ENDCG
+        }
+    }
+}
